111f160540b9902c64ec6b85174a56b5f9b25758
Lorne Martin <lorne@horstwelding.com>
2018-04-03 15:41:54 -0400

    VaultItemProcessor - Grouped API calls together into one so speed is much improved.

--- ItemExport/ItemExportCommandExtension.cs
+++ ItemExport/ItemExportCommandExtension.cs
@@ -880,11 +880,6 @@ namespace ItemExport
             notesPair.ToName = "Notes";
             notesPair.FromName = "0d012a5c-cc28-443c-b44e-735372eee117";
 
-            //MapPair primaryLinkPair = new MapPair();
-            //primaryLinkPair.ToName = "Primary File Name";
-            //primaryLinkPair.FromName = "XRefId";
-            //primaryLinkPair.FromName = "File Name01-{91E61FA5-5660-47DA-BDC4-DC3A620C4CEC}";
-
             MapPair revisionNumberPair = new MapPair();
             revisionNumberPair.ToName = "Revision";
             revisionNumberPair.FromName = "Revision";
@@ -908,6 +903,22 @@ namespace ItemExport
                 }
             }
 
+            // create a list to hold all the IDs of the BOM items
+            List<long> idList = new List<long>();
+
+            // create a dictionary to match up the exported ids with the exported item numbers
+            Dictionary<string, long> bomDict = new Dictionary<string, long>();
+
+            // loop through all the bomItems and extract the IDs along with the item numbers
+            foreach(var v in pkgBom.PkgItemArray)
+            {
+                idList.Add(v.ID);
+                bomDict.Add(v.ItemNum, v.ID);
+            }
+
+            // now create a list to store all the BOM items in
+            List<BOMComp> bomList = new List<BOMComp>();
+
             // we now need to replace the item number with the primary file name field.  I can't figure out how to map it above, so we have to open the text file,
             // search through it line by line, and query the vault for the primary file name link of of each item number.  We then save the text file again with the
             // primary file name now in place of the item number....
@@ -927,13 +938,32 @@ namespace ItemExport
 
                     string[] items = line.Split('\t');
                     string origItemNumber = items[1];
-                    string revsionNumber = items[19];
+                    //string revsionNumber = items[19];
 
-                    Item searchItem = connection.WebServiceManager.ItemService.GetItemByItemNumberAndRevisionNumber(origItemNumber, revsionNumber);
-                    BOMComp bc = connection.WebServiceManager.ItemService.GetPrimaryComponentsByItemIds(new long[] { searchItem.Id }).First();
-                    long primaryLinkID = bc.XRefId;
+                    // download all the bom items into a list in one API call
+                    bomList = connection.WebServiceManager.ItemService.GetPrimaryComponentsByItemIds(idList.ToArray()).ToList();
+
+                    // create another dictionary to match up the IDs with the bomComps
+                    Dictionary<long, BOMComp> bomDict2 = new Dictionary<long, BOMComp>();
+
+                    // populate the second dictionary
+                    int index = 0;
+                    foreach(long l in idList)
+                    {
+                        bomDict2.Add(l, bomList[index]);
+                        index++;
+                    }
+
+                    long searchID = 0;
+                    bomDict.TryGetValue(origItemNumber, out searchID);
+
+                    BOMComp searchbomComp = new BOMComp();
+                    bomDict2.TryGetValue(searchID, out searchbomComp);
+
+                    long primaryLinkID = searchbomComp.XRefId;  // get the id of the primary linked file
                     if (primaryLinkID != -1)    // if we have a primary file name, use it
                     {
+                        // we could likely speed this up some more if we grouped this all into one call rather than one by one, but code would get messier yet...
                         Autodesk.Connectivity.WebServices.File primaryLinkFile = connection.WebServiceManager.DocumentService.GetFileById(primaryLinkID);
                         string primaryLinkName = primaryLinkFile.Name;
 
