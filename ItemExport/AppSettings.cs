180d6edd750cbdb478ce53268b174a259d51b5d9
Lorne Martin <lorne@horstwelding.com>
2019-02-11 16:41:22 -0500

    ItemExport - WIP changing over to use common config file, not working yet.

--- ItemExport/AppSettings.cs
+++ ItemExport/AppSettings.cs
@@ -15,8 +15,8 @@ using System.Xml;
 
 public static class AppSettings
 {
-    public static readonly string SettingsFilePath = new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName + @"\AppSettings.xml";
-
+    //public static readonly string SettingsFilePath = new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName + @"\AppSettings.xml";
+    public static readonly string SettingsFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "VaultExtensions", "AppSettings.xml");
     #region Get setting
     public static object Get(string key)
     {
