27ce02fb55488a185eabe3148dc3d9d0288c4491
Lorne Martin <lorne@horstwelding.com>
2020-09-10 16:13:34 -0400

    Added new project to test retrieving a drawing from vault  on demand

---
+++ VaultDrawingRetrieveTest/VaultDrawingRetrieveTest/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VaultAccess;
+
+namespace VaultDrawingRetrieveTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fileName = "CHC - 04";
+
+            VaultAccess.VaultAccess hlaVault = new VaultAccess.VaultAccess();
+
+            hlaVault.Login("lorne", "lorne", "hwvsvt01", "Vault");
+
+            Dictionary<long, string> idwDict = new Dictionary<long, string>();
+            hlaVault.GetIDWsAssociatedWithModelByVaultName(fileName);
+
+        }
+    }
+}
