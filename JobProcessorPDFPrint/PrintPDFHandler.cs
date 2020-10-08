e55ab98f7b8555e0ec1da092c4a1e9c1d6f4321e
Lorne Martin <lorne@horstwelding.com>
2020-10-08 13:46:34 -0400

    WIP - JobProcessorPrintPDF and PrintPDFVaultExtension updated to use Microsoft PDF Printer.  Not working yet.

--- JobProcessorPDFPrint/PrintPDFHandler.cs
+++ JobProcessorPDFPrint/PrintPDFHandler.cs
@@ -318,7 +318,8 @@ namespace JobProcessorPrintPDF
                     string errMsg = "";
                     string logMsg = "";
                     //if (printOb.printToPDFNew(fileName, propDict, PDFPath, ref errMsg, ref logMsg))
-                    if (printOb.printToPDF(fileName,PDFPath,pdfPrinterName, psToPdfProgName,ghostScriptWorkingFolder, ref errMsg, ref logMsg))
+                    //if (printOb.printToPDF(fileName,PDFPath,pdfPrinterName, psToPdfProgName,ghostScriptWorkingFolder, ref errMsg, ref logMsg))
+                    if (printOb.printToPDF(fileName, PDFPath, pdfPrinterName, ref errMsg, ref logMsg))
                     {
                         logMessage += logMsg;
                         return true;
