c3ce9d6e7b0c9275a20c2a6f5c08f502389c6efd
Lorne Martin <lorne@horstwelding.com>
2020-10-16 11:12:40 -0400

    VaultItemProcessor - Ready to release as 2020.1

--- VaultItemProcessor/VaultItemProcessor/Properties/AssemblyInfo.cs
+++ VaultItemProcessor/VaultItemProcessor/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vault Item Processor 2020")]
+[assembly: AssemblyTitle("Vault Item Processor 2020.1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vault Item Processor 2020")]
+[assembly: AssemblyProduct("Vault Item Processor 2020.1")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
