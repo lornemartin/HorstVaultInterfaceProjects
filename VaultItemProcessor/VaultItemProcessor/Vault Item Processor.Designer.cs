f7bd176ec40ec41487acf8f00ce7623c22c409e3
Lorne Martin <lorne@horstwelding.com>
2021-10-09 07:15:06 -0400

    Updated VaultItemProcessor to 2022

--- VaultItemProcessor/VaultItemProcessor/Vault Item Processor.Designer.cs
+++ VaultItemProcessor/VaultItemProcessor/Vault Item Processor.Designer.cs
@@ -439,7 +439,7 @@
             this.toolStripStatusLabel1});
             this.statusStrip1.Location = new System.Drawing.Point(0, 756);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1752, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(1284, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -649,7 +649,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1752, 778);
+            this.ClientSize = new System.Drawing.Size(1284, 778);
             this.Controls.Add(this.radioGroup2);
             this.Controls.Add(this.radioGroup1);
             this.Controls.Add(this.groupBoxInput);
@@ -659,7 +659,7 @@
             this.Controls.Add(this.groupBoxOutput);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Vault Item Processor 2021";
+            this.Text = "Vault Item Processor 2022";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.repositoryItemTextEdit1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.exportTreeList)).EndInit();
