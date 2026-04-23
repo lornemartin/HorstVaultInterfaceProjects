7bb229b1a2aeb6e801a62482c38ed04f1b31ec74
Lorne Martin <lorne@compassfoundation.io>
2026-04-23 06:14:48 -0400

    add paging and date ranged loading to production grids

---
+++ HorstMFG/src/HorstMFG.Infrastructure/Data/Migrations/20260423100302_AddEntryDateIndexes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HorstMFG.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEntryDateIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE INDEX IF NOT EXISTS ix_nest_orders_entry_date ON nest_orders (\"EntryDate\")");
+            migrationBuilder.Sql("CREATE INDEX IF NOT EXISTS ix_nest_batches_entry_date ON nest_batches (\"EntryDate\")");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP INDEX IF EXISTS ix_nest_orders_entry_date");
+            migrationBuilder.Sql("DROP INDEX IF EXISTS ix_nest_batches_entry_date");
+
+        }
+    }
+}
